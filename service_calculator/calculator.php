7f870c66f21a98c90b565972e4b8741f1c038dd7
SaintPath <37582704+SaintPath@users.noreply.github.com>
2018-11-14 01:04:24 +0200

    Another bug
    I had an "Enter" after the ?> in the end of the program.
    and added elses

--- service_calculator/calculator.php
+++ service_calculator/calculator.php
@@ -6,12 +6,13 @@ class GetParams{
     public $num4;
     public $func;
     public $method;
+    public $res;
     
     public function __construct(){
-        $num1 = 0;
-        $num2 = 0; 
-        $num3 = 0;
-        $num4 = 0;
+        $this->num1 = 0;
+        $this->num2 = 0; 
+        $this->num3 = 0;
+        $this->func = 'sum';
     }
 
     public function pramas() {
@@ -19,56 +20,68 @@ class GetParams{
         case 'POST':
             if(isset($_POST["num1"])) 
                 $this->num1 = (int)$_POST["num1"];
+            else  $this->num1 = 0;
             if(isset($_POST["num2"])) 
-                $this->num2 = (int)$_POST["num2"]; 
+                $this->num2 = (int)$_POST["num2"];
+            else  $this->num2 = 0;
             if(isset($_POST["num3"])) 
                 $this->num3 = (int)$_POST["num3"];
+            else  $this->num3 = 0;
             if(isset($_POST["func"])) 
-                $this->func = $_POST["func"];
+                $this->func = (string) $_POST["func"];
+            else  $this->func = "sum";
+            
             break;
 
         case 'GET':
             if(isset($_GET["num1"])) 
                 $this->num1 = (int)$_GET["num1"]; 
+            else  $this->num1 = 0;
             if(isset($_GET["num2"])) 
                 $this->num2 = (int)$_GET["num2"]; 
+            else  $this->num2 = 0;
             if(isset($_GET["num3"])) 
-                $this->num3 = (int)$_GET["num3"]; 
+                $this->num3 = (int)$_GET["num3"];
+            else  $this->num3 = 0;
             if(isset($_GET["func"])) 
-                $this->func = $_GET["func"];
+                $this->func = (string) $_GET["func"];
+            else  $this->func = "sum";
+            
             break;
             
         case 'PUT':
             parse_str(file_get_contents("php://input"),$_PUT);
             if(isset($_PUT["num1"])) 
-                $this->num1 = (int)$_PUT["num1"]; 
+                $this->num1 = (int)$_PUT["num1"];
+            else  $this->num1 = 0;
             if(isset($_PUT["num2"]))            
                 $this->num2 = (int)$_PUT["num2"]; 
+            else  $this->num2 = 0;
             if(isset($_PUT["num3"]))       
                 $this->num3 = (int)$_PUT["num3"]; 
+            else  $this->num3 = 0;
             if(isset($_PUT["func"]))         
-                $this->func = $_PUT["func"];
-            break;
+                $this->func = (string)$_PUT["func"];
+            else  $this->func = "sum";
             
+            break;
         }
     }
 
     public function calculate(){
         switch ($this->func){
             case 'sum':
-                return $this->num1 + $this->num2 + $this->num3;
+                $this->res = $this->num1 + $this->num2 + $this->num3;
                 break;
             case 'avg':
-                return ($this->num1 + $this->num2 + $this->num3) / 3;
+                $this->res = ($this->num1 + $this->num2 + $this->num3) / 3;
                 break;
             case 'mult':
-                return $this->num1 * $this->num2 * $this->num3;
+                $this->res = $this->num1 * $this->num2 * $this->num3;
                 break;
             default:
-                return 'Error!';
+                return 'func Error!';
         }
     }
 }
 ?>
-
- 
--- service_calculator/index.php
+++ service_calculator/index.php
@@ -1,24 +1,29 @@
 <?php 
     include "calculator.php";
     
-    if ($_SERVER['REQUEST_METHOD'] != 'POST' && $_SERVER['REQUEST_METHOD'] != 'GET' && $_SERVER['REQUEST_METHOD'] != 'PUT')
+    if ($_SERVER['REQUEST_METHOD'] != 'POST' && $_SERVER['REQUEST_METHOD'] != 'GET' && $_SERVER['REQUEST_METHOD'] != 'PUT'){
+        $retVal = 'Error';
+        $a = array('retVal'=>$retVal);
+        header('Content-Type: application/json');
+        echo json_encode($a);
         return;
+    }
 
     $calc = new GetParams;
     $calc->method = $_SERVER['REQUEST_METHOD'];
-
+    
     if (isset($calc->method))
         $calc->pramas();
     
-    $res = $calc->calculate();
+    $calc->calculate();
     
     // build the results Array
-    $a = array('retVal' => $res);
+    $a = array('retVal' => $calc->res);
     
     // set header for json response
     header('Content-Type: application/json'); 
 
     // echo the converted JSON Object from the Array
-    echo json_encode($a);
+   echo json_encode($a);
 
 ?>
